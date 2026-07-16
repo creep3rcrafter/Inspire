a9ea45cc0ea3d3248d04f2369236629326b92653
Brenden Delano <brendendelano@gmail.com>
2026-07-16 01:05:53 -0600

    Render & Color Cleanup

--- common/src/main/java/net/github/creep3rcrafter/inspire/client/register/InspireColors.java
+++ common/src/main/java/net/github/creep3rcrafter/inspire/client/register/InspireColors.java
@@ -5,10 +5,16 @@ import net.github.creep3rcrafter.inspire.block.BluestoneWireBlock;
 import net.github.creep3rcrafter.inspire.block.ChromaticLampBlock;
 import net.github.creep3rcrafter.inspire.block.SoulGlassBlock;
 import net.github.creep3rcrafter.inspire.register.InspireBlocks;
+import net.github.creep3rcrafter.inspire.register.InspireItems;
 import net.minecraft.client.color.block.BlockColor;
+import net.minecraft.client.color.item.ItemColor;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.MapColor;
+import org.jetbrains.annotations.NotNull;
 
 public class InspireColors {
     public static final BlockColor BLUESTONE_WIRE;
@@ -45,5 +51,28 @@ public class InspireColors {
         ColorHandlerRegistry.registerBlockColors(BLUESTONE_WIRE, InspireBlocks.BLUESTONE_WIRE.get());
         ColorHandlerRegistry.registerBlockColors(SOUL_GLASS, InspireBlocks.SOUL_GLASS.get());
         //ColorHandlerRegistry.registerBlockColors(CHROMATIC_LAMP, InspireBlocks.SOUL_GLASS.get());
+    }
+    public static ItemColor POTION_JAR;
+    public static ItemColor SOUL_GLASS_ITEM;
+
+    static {
+        POTION_JAR = new ItemColor() {
+            @Override
+            public int getColor(@NotNull ItemStack itemStack, int i) {
+                return i == 0 ? itemStack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY).getColor() : -1;
+            }
+        };
+        SOUL_GLASS_ITEM = new ItemColor() {
+            @Override
+            public int getColor(@NotNull ItemStack itemStack, int i) {
+                //int brightness = itemStack.getOrCreateTag().getCompound("BlockStateTag").getInt("brightness");
+                float r = 0.5F;
+                float g = (0.4f + 1) * 0.6f;
+                float b = (0.4f + 1) * 0.7f;
+                return ((int) (r * 255) & 0xFF) << 16 | ((int) (g * 255) & 0xFF) << 8 | ((int) (b * 255) & 0xFF);
+            }
+        };
+        ColorHandlerRegistry.registerItemColors(POTION_JAR, InspireItems.POTION_JAR.get());
+        ColorHandlerRegistry.registerItemColors(SOUL_GLASS_ITEM, InspireItems.SOUL_GLASS.get());
     }
 }
--- common/src/main/java/net/github/creep3rcrafter/inspire/client/register/InspireRenderTypes.java
+++ common/src/main/java/net/github/creep3rcrafter/inspire/client/register/InspireRenderTypes.java
@@ -3,6 +3,7 @@ package net.github.creep3rcrafter.inspire.client.register;
 import dev.architectury.registry.client.rendering.RenderTypeRegistry;
 import net.github.creep3rcrafter.inspire.register.InspireBlocks;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.material.Fluids;
 
 public class InspireRenderTypes {
     static {
@@ -17,5 +18,12 @@ public class InspireRenderTypes {
         RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.CRIMSON_NYLIUM_SHELF.get());
         RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.ICICLE.get());
         RenderTypeRegistry.register(RenderType.translucent(), InspireBlocks.THIN_ICE.get());
+        RenderTypeRegistry.register(RenderType.translucent(), Fluids.LAVA);//performance hit oof
+        RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.FILTER.get());
+        RenderTypeRegistry.register(RenderType.translucent(), InspireBlocks.SOUL_GLASS.get());
+        RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.WITHERED_DOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.WITHERED_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.WITHERED_SAPLING.get());
+        RenderTypeRegistry.register(RenderType.cutout(), InspireBlocks.POTTED_WITHERED_SAPLING.get());
     }
 }
